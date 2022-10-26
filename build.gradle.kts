ea8a6776dda1dc84dbfb70691e89d96c920f8122
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-10-26 18:52:09 +0200

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.configurationcache.extensions.capitalized
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "xyz.xenondevs.nova"
-version = "0.2"
+version = "0.2.1"
 
 val mojangMapped = System.getProperty("mojang-mapped") != null
 
