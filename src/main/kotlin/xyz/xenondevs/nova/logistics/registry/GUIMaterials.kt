6228f5f42a4833b329dab6b531ea984ec876eb44
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-12-28 22:15:59 +0100

    Adjust to Nova changes

--- src/main/kotlin/xyz/xenondevs/nova/logistics/registry/GUIMaterials.kt
+++ src/main/kotlin/xyz/xenondevs/nova/logistics/registry/GUIMaterials.kt
@@ -1,15 +1,15 @@
 package xyz.xenondevs.nova.logistics.registry
 
 import xyz.xenondevs.nova.logistics.Logistics
-import xyz.xenondevs.nova.material.NovaMaterialRegistry.registerUnnamedItem
+import xyz.xenondevs.nova.material.NovaMaterialRegistry.registerUnnamedHiddenItem
 
 object GUIMaterials {
     
-    val ITEM_FILTER_PLACEHOLDER = registerUnnamedItem(Logistics, "gui_item_filter_placeholder")
-    val TRASH_CAN_PLACEHOLDER = registerUnnamedItem(Logistics, "gui_trash_can_placeholder")
-    val NBT_BTN_OFF = registerUnnamedItem(Logistics, "gui_nbt_btn_off")
-    val NBT_BTN_ON = registerUnnamedItem(Logistics, "gui_nbt_btn_on")
-    val WHITELIST_BTN = registerUnnamedItem(Logistics, "gui_whitelist_btn")
-    val BLACKLIST_BTN = registerUnnamedItem(Logistics, "gui_blacklist_btn")
+    val ITEM_FILTER_PLACEHOLDER = registerUnnamedHiddenItem(Logistics, "gui_item_filter_placeholder")
+    val TRASH_CAN_PLACEHOLDER = registerUnnamedHiddenItem(Logistics, "gui_trash_can_placeholder")
+    val NBT_BTN_OFF = registerUnnamedHiddenItem(Logistics, "gui_nbt_btn_off")
+    val NBT_BTN_ON = registerUnnamedHiddenItem(Logistics, "gui_nbt_btn_on")
+    val WHITELIST_BTN = registerUnnamedHiddenItem(Logistics, "gui_whitelist_btn")
+    val BLACKLIST_BTN = registerUnnamedHiddenItem(Logistics, "gui_blacklist_btn")
     
 }
